61a744624be37f9f928e13343d9a54b7f71ef93b
VijayaMan2023 <Newpass2023>
2023-08-26 18:16:52 +0530

    Second commit

--- src/Demo/Gitfirstprog.java
+++ src/Demo/Gitfirstprog.java
@@ -1,9 +1,17 @@
 package Demo;
 
 public class Gitfirstprog {
+	
+	void sum() {
+		int a=10;
+		int b=20;
+		int sum;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello Vijaya");
+		
+		
 
 	}
 
---
+++ src/Demo/Gitsecondprog.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Gitsecondprog {
+
+	public static void main(String[] args) {
+		System.out.println("My secong program");
+
+	}
+
+}
