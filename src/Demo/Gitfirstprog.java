e0c5910273f2b65c527deb3e384f36bae7831dd8
VijayaMan2023 <Newpass2023>
2023-08-26 18:22:10 +0530

    third commit

--- src/Demo/Gitfirstprog.java
+++ src/Demo/Gitfirstprog.java
@@ -1,18 +1,25 @@
 package Demo;
 
 public class Gitfirstprog {
+	int a=10;
+	int b=20;
+	int sum;
 	
 	void sum() {
-		int a=10;
-		int b=20;
-		int sum;
+		sum = a + b;
+		System.out.println("Sum is : " + sum);
+		
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello Vijaya");
+		Gitfirstprog obj = new Gitfirstprog();
+		
+		obj.sum();
+		
+		
 		
 		
 
 	}
-
 }
